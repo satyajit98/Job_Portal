20b75ad376cb2b236f8585815d59a6fdab8bfec3
satyajit98 <mesatya2000@gmail.com>
2024-10-14 12:10:50 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
 
@@ -14,12 +17,13 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello from job portal", success: true });
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port: ${PORT}`);
 });
